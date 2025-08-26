77f16ddcff95f61d748e647ea5da034d00c8ec78
gopanmodak <gopanhridoy@gmail.com>
2025-08-26 12:18:17 +0600

    first commit

---
+++ Level-1/javascript.js
@@ -0,0 +1,43 @@
+
+/*
+const studentInfo = {
+    name : 'Gopan Kumer Modak',
+    id: 1931237,
+    cgpa : 3.10,
+    department : 'CSE',
+    homeTown : 'Tangail',
+    currentAddress : 'Senpara,Mirpur-10',
+    workAt : 'Dream71',
+    position : 'intern',
+    favGame :'Football & Cricket'
+}
+
+console.log(studentInfo);
+console.log ('Name : ' + studentInfo.name);
+console.log ('ID: '+ studentInfo['id']);
+console.log ('Work at : ' + studentInfo['workAt'])
+
+
+
+/*
+
+let number = 5;
+let squreNumber =number*number;
+console.log('\n\n\nSqure Number is : ' + squreNumber);   //Print in Old Model.
+
+let number1=parseInt(prompt('Enter a number'));
+let squreNumber1=number1*number1;
+console.log(`Squre Number Is :  ${squreNumber1}`);  //Print in dollar ($)
+
+*/
+
+
+let a=5;
+let b=3;
+
+console.log('a=', a, '& b=', b);
+console.log('a + b =' , a+b);
+console.log('a - b =' , a-b);
+console.log('a * b =' , a*b);
+console.log('a / b =' , a/b);
+console.log('a % b =' , a%b);

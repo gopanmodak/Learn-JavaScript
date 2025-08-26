0cfe1e756025d6e68ee4b0d18f714f05d4714978
gopanmodak <gopanhridoy@gmail.com>
2025-08-26 15:09:04 +0600

    Learn JavaScript

--- Level-1/javascript.js
+++ Level-1/javascript.js
@@ -1,5 +1,5 @@
+/* 
 
-/*
 const studentInfo = {
     name : 'Gopan Kumer Modak',
     id: 1931237,
@@ -19,7 +19,7 @@ console.log ('Work at : ' + studentInfo['workAt'])
 
 
 
-/*
+
 
 let number = 5;
 let squreNumber =number*number;
@@ -29,15 +29,58 @@ let number1=parseInt(prompt('Enter a number'));
 let squreNumber1=number1*number1;
 console.log(`Squre Number Is :  ${squreNumber1}`);  //Print in dollar ($)
 
-*/
+ */
+
+
+/* let a=7;
+let b=3;
+ */
+/* console.log('a=', a, '& b=', b);
+console.log('a + b =' , a+b);
+console.log('a - b =' , a-b);
+console.log('a * b =' , a*b);
+console.log('a / b =' , a/b);
+console.log('a % b =' , a%b);  //The Modulo Operator
+console.log('a ** b =' , a**b);   //The Exponentiation Operator 
+
+
+//Unary Operator
+a++;
+console.log('a ++ =' , a);   //The Exponentiation Operator 
+b--;
+console.log('b-- =' , b);
+++a;
+console.log('++a =' , a);
+--b;
+console.log('--b =' , b);
+ */
+
+
+//Asignment Operator
+/* a+=4;
+console.log('a=',a);
+a-=4;
+console.log('a=',a);
+a*=4;
+console.log('a=',a);
+a/=4;
+console.log('a=',a);
+a**=4;
+console.log('a=',a);
+a%=4;
+console.log('a=',a); */
+
+
+
+
 
 
 let a=5;
 let b=3;
 
-console.log('a=', a, '& b=', b);
-console.log('a + b =' , a+b);
-console.log('a - b =' , a-b);
-console.log('a * b =' , a*b);
-console.log('a / b =' , a/b);
-console.log('a % b =' , a%b);
+console.log('a>b' ,a>b)
+console.log('a<b' ,a<b)
+console.log('a>=b' ,a>=b)
+console.log('a<=b' ,a<=b)
+
+

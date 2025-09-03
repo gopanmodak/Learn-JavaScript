76ee236fe906580196a182bdc23d5c7546e2c94d
gopanmodak <gopanhridoy@gmail.com>
2025-09-03 18:02:31 +0600

    Added

--- All-Practice/practice.js
+++ All-Practice/practice.js
@@ -21,6 +21,88 @@ companies.shift();
 companies.push("Amazon");
 console.log(companies);
 
+
+
+function countVowels(str){
+    let count = 0;
+    for (let char of str){
+        if(char==='a'||char==='e'||char==='i'||char==='o'||char==='u') {
+            count++;
+        }
+    }
+    return count;
+}
 */
 
 
+
+
+const countVowels=(str)=>{
+
+    let count = 0;
+for (let char of str){
+   if(char==='a'||char==='e'||char==='i'||char==='o'||char==='u') {
+      count++;
+   }
+}
+return count;
+
+
+}
+
+function isEven (num){
+    if (num%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let num2=isEven(24)
+console.log(num2);
+
+const isEven1=(num)=>{
+    if (num%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let num3=isEven1(25)
+console.log(num3);
+
+
+
+function findMax(num1,num2,num3){
+    if (num1>num2 && num1>num3){
+        return num1;
+    }
+    else if (num2>num3 && num2>num1){
+        return num2
+    }
+    else{
+        return num3;
+    }
+        
+}
+let maxNumber=findMax(10,40,30)
+console.log(maxNumber);
+
+
+const findMax1=(num1,num2,num3)=>{
+    if (num1>num2 && num1>num3){
+        return num1;
+    }
+    else if (num2>num3 && num2>num1){
+        return num2
+    }
+    else{
+        return num3;
+    }
+        
+}
+let maxNumber1=findMax1(10,40,30)
+console.log(maxNumber1);
--- Level-4/page.js
+++ Level-4/page.js
@@ -34,3 +34,33 @@ console.log(sum);
 
 
 //Sum Function using Function Expression
+
+
+const sum=(x,y)=>{
+    console.log(`Sum is : ${x+y}`);
+
+}
+sum(20,15);
+const div=(x,y)=>{
+    console.log(`Div is : ${x/y}`);
+
+}
+div(20,15);
+const mult=(x,y)=>{
+    console.log(`Multi is : ${x*y}`);
+
+}
+mult(20,15);
+const sub=(x,y)=>{
+    console.log(`Sub is : ${x-y}`);
+
+}
+sub(20,15);
+
+
+const arrowSum=(x,y)=>{
+    return x+y;
+}
+console.log(arrowSum(20,15));
+
+
